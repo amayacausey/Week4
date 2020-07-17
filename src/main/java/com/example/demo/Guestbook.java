a23d582a60493dc9b5288fd7d5a960cbe7093a9c
amayacausey <amayacausey@gmail.com>
2020-07-17 07:38:24 -0700

    working but need to style, delete button,nav bar

---
+++ src/main/java/com/example/demo/Guestbook.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+public class Guestbook {
+    private String name;
+    private String date;
+    private String wishes;
+
+    public Guestbook() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getWishes() {
+        return wishes;
+    }
+
+    public void setWishes(String wishes) {
+        this.wishes = wishes;
+    }
+}
---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+
+@Controller
+public class HomeController{
+    ArrayList<Guestbook> allGuestbook = new ArrayList<Guestbook>();
+
+    @RequestMapping("/")
+    public String loadGuestForm(Model model){
+        model.addAttribute("guestbook", new Guestbook());
+        return "addGuestbook";
+    }
+
+    @PostMapping("/confirmGuestbook")
+    public String confirmGuestbook(@ModelAttribute Guestbook guestbook,Model model)
+    {
+        allGuestbook.add(guestbook);
+        model.addAttribute("allGuestbook",allGuestbook);
+        return "listAll";
+    }
+    @RequestMapping("/listAll")
+    public String listAllGuestbook (Model model)
+    {
+        model.addAttribute("allGuestbook",allGuestbook);
+        return "listAll";
+    }
+
+}
